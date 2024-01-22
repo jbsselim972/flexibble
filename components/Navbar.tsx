84452f4335c28ea29568b800cb12188bd63e0941
jbsselim <jbsselim@gmail.com>
2024-01-22 18:28:12 -0400

    changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
-import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import AuthProviders from "./AuthProviders";
+
+import { NavLinks } from "@/constants";
 import { getCurrentUser } from "@/lib/session";
-import { signOut } from "next-auth/react";
+import AuthProviders from "./AuthProviders";
 import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
